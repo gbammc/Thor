7fd5232f76738633e227d6a1418b5ce9bf61e9c2
AlvinZhu <gbammc@gmail.com>
2014-12-02 22:51:49 +0800

    Note: add apps list view

---
+++ Thor/View/AZAppsListView.h
@@ -0,0 +1,13 @@
+//
+//  AZAppsListView.h
+//  Thor
+//
+//  Created by Alvin on 12/2/14.
+//  Copyright (c) 2014 AlvinZhu. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AZAppsListView : NSView
+
+@end
---
+++ Thor/View/AZAppsListView.m
@@ -0,0 +1,19 @@
+//
+//  AZAppsListView.m
+//  Thor
+//
+//  Created by Alvin on 12/2/14.
+//  Copyright (c) 2014 AlvinZhu. All rights reserved.
+//
+
+#import "AZAppsListView.h"
+
+@implementation AZAppsListView
+
+- (void)drawRect:(NSRect)dirtyRect {
+    [super drawRect:dirtyRect];
+    
+    // Drawing code here.
+}
+
+@end
