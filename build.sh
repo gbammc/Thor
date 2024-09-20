a56b9ae90650068e4d02d4931b3dc01a35b7ab6c
alvinzhu <gbammc@gmail.com>
2024-09-20 23:26:32 +0800

    feat: v1.5.16

--- build.sh
+++ build.sh
@@ -2,26 +2,35 @@
 
 APP_NAME="Thor"
 
-# build
+# build ARM
+xcodebuild -workspace "$APP_NAME.xcworkspace" -scheme "$APP_NAME" -archivePath "${APP_NAME}_arm64" -arch arm64 archive
 
-xcodebuild -workspace "$APP_NAME.xcworkspace" -scheme "$APP_NAME" -archivePath "$APP_NAME" archive
+# build Intel
+xcodebuild -workspace "$APP_NAME.xcworkspace" -scheme "$APP_NAME" -archivePath "${APP_NAME}_x86_64" -arch x86_64 archive
 
-xcodebuild -exportArchive -archivePath "$APP_NAME".xcarchive -exportPath . -exportOptionsPlist ExportOptions.plist
+# export ARM
+xcodebuild -exportArchive -archivePath "${APP_NAME}_arm64.xcarchive" -exportPath ./arm64 -exportOptionsPlist ExportOptions.plist
+
+# export Intel
+xcodebuild -exportArchive -archivePath "${APP_NAME}_x86_64.xcarchive" -exportPath ./x86_64 -exportOptionsPlist ExportOptions.plist
+
+# lipo combine
+lipo -create ./arm64/"$APP_NAME".app/Contents/MacOS/"$APP_NAME" ./x86_64/"$APP_NAME".app/Contents/MacOS/"$APP_NAME" -output ./arm64/"$APP_NAME".app/Contents/MacOS/"$APP_NAME"
 
 sleep 3
 
-# archive
+# Archive
 
-VERSION=`mdls -name kMDItemVersion "$APP_NAME".app | grep -o '\d*\.\d*\.\d*'`
+VERSION=`mdls -name kMDItemVersion ./arm64/"$APP_NAME".app | grep -o '\d*\.\d*\.\d*'`
 
-zip -r "$APP_NAME".zip "$APP_NAME".app
+zip -r ./arm64/"$APP_NAME".zip ./arm64/"$APP_NAME".app
 
-mv "$APP_NAME".zip Releases/"$APP_NAME"_"$VERSION".zip
+mv ./arm64/"$APP_NAME".zip Releases/"$APP_NAME"_"$VERSION".zip
 
-# sparkle
+# Sparkle
 
 ./Pods/Sparkle/bin/generate_appcast ./Releases
 
-# clean
+# Clean
 
-rm -rf "$APP_NAME".app "$APP_NAME".xcarchive DistributionSummary.plist Packaging.log Releases/.tmp
+rm -rf "${APP_NAME}_arm64.xcarchive" "${APP_NAME}_x86_64.xcarchive" ./arm64 ./x86_64 Releases/.tmp
